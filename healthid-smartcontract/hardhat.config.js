865d000ea30607c81dd328d7faa0d901b1d7c149
Robson Inoue <robson.inoue@gmail.com>
2023-09-20 10:13:53 -0300

    hardhat config

--- healthid-smartcontract/hardhat.config.js
+++ healthid-smartcontract/hardhat.config.js
@@ -1,6 +1,17 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-ethers");
+
+const mnemonic = "test test test test test test test test test test test test"
+const accounts = {
+  mnemonic
+}
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
+  networks : {
+    zeniq: {
+      url: "https://smart1.zeniq.network:9545",
+      accounts
+    }
+  }
 };
